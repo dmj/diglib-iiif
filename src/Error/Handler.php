7ac109db67f33e401171bab7c9be6aa3dc09cfbf
David Maus <maus@hab.de>
2018-12-06 15:08:08 +0100

    Implement custom error handler

---
+++ src/Error/Handler.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of HAB Diglib IIIF.
+ *
+ * HAB Diglib IIIF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HAB Diglib IIIF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HAB Diglib IIIF.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author    David Maus <maus@hab.de>
+ * @copyright (c) 2018 by Herzog August Bibliothek Wolfenbüttel
+ * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public License v3 or higher
+ */
+
+namespace HAB\Diglib\API\Error;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+use Exception;
+
+/**
+ * Error handler.
+ *
+ * @author    David Maus <maus@hab.de>
+ * @copyright (c) 2018 by Herzog August Bibliothek Wolfenbüttel
+ * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public License v3 or higher
+ */
+class Handler
+{
+    public function __invoke (Request $request, Response $response, Exception $exception)
+    {
+        if (!$exception instanceof Http) {
+            $exception = new Http(500, array(), $exception);
+        }
+        foreach ($exception->getHeaders() as $name => $value) {
+            $response = $response->withHeader($name, $value);
+        }
+        $response = $response->withStatus($exception->getCode());
+        $response = $response->write(
+            sprintf('%03d %s', $response->getStatusCode(), $response->getReasonPhrase())
+        );
+        return $response;
+    }
+}
---
+++ src/Error/Http.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of HAB Diglib IIIF.
+ *
+ * HAB Diglib IIIF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HAB Diglib IIIF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HAB Diglib IIIF.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author    David Maus <maus@hab.de>
+ * @copyright (c) 2018 by Herzog August Bibliothek Wolfenbüttel
+ * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public License v3 or higher
+ */
+
+namespace HAB\Diglib\API\Error;
+
+use Exception;
+
+/**
+ * Signal an error on the HTTP transport level.
+ *
+ * @author    David Maus <maus@hab.de>
+ * @copyright (c) 2018 by Herzog August Bibliothek Wolfenbüttel
+ * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public License v3 or higher
+ */
+class Http extends Exception
+{
+    public function __construct ($code, array $headers = array(), Exception $previous = null)
+    {
+        parent::__construct('Internal Server Error', $code, $previous);
+        $this->headers = $headers;
+    }
+
+    public function getHeaders ()
+    {
+        return $this->headers;
+    }
+}
