516bc580000353685a342e8306a2b1cad7e09a47
David Maus <maus@hab.de>
2018-12-11 12:46:02 +0100

    Add trait for LoggerAware classes

---
+++ src/LoggerAwareTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * This file is part of HAB Diglib IIIF.
+ *
+ * HAB Diglib IIIF is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HAB Diglib IIIF is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HAB Diglib IIIF.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author    David Maus <maus@hab.de>
+ * @copyright (c) 2018 by Herzog August Bibliothek Wolfenbüttel
+ * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public License v3 or higher
+ */
+
+namespace HAB\Diglib\API;
+
+use Psr\Log\LoggerInterface;
+
+/**
+ * Logger trait.
+ *
+ * @author    David Maus <maus@hab.de>
+ * @copyright (c) 2018 by Herzog August Bibliothek Wolfenbüttel
+ * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public License v3 or higher
+ */
+trait LoggerAwareTrait
+{
+    public function setLogger (LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function getLogger ()
+    {
+        if (!$this->logger) {
+            $this->setLogger(new NullLogger());
+        }
+        return $this->logger;
+    }
+
+    protected function log ($level, $message, array $context = array())
+    {
+        $this->getLogger()->log($level, $message, $context);
+    }
+}
