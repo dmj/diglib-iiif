544c9517cd49bc8dd829104972e22e923bfbec66
David Maus <maus@hab.de>
2018-12-11 15:05:58 +0100

    Change route, base on /iiif/

--- config/routes.php
+++ config/routes.php
@@ -1,46 +1,46 @@
 <?php
 
-$app->get('/{objectId}/manifest.json', 'IIIF.Manifest:asJSON')
+$app->get('/iiif/{objectId}/manifest.json', 'IIIF.Manifest:asJSON')
     ->setName('iiif.manifest.json')
     ->add('IIIF.Filter');
 
-$app->get('/{objectId}/manifest', 'IIIF.Manifest')
+$app->get('/iiif/{objectId}/manifest', 'IIIF.Manifest')
     ->setName('iiif.manifest')
     ->add('IIIF.Filter');
 
-$app->get('/{objectId}/canvas/{entityId}.json', 'IIIF.Canvas:asJSON')
+$app->get('/iiif/{objectId}/canvas/{entityId}.json', 'IIIF.Canvas:asJSON')
     ->setName('iiif.canvas.json')
     ->add('IIIF.Filter');
 
-$app->get('/{objectId}/canvas/{entityId}', 'IIIF.Canvas')
+$app->get('/iiif/{objectId}/canvas/{entityId}', 'IIIF.Canvas')
     ->setName('iiif.canvas')
     ->add('IIIF.Filter');
 
-$app->get('/{objectId}/annotation/{entityId}.json', 'IIIF.Annotation:asJSON')
+$app->get('/iiif/{objectId}/annotation/{entityId}.json', 'IIIF.Annotation:asJSON')
     ->setName('iiif.annotation.json')
     ->add('IIIF.Filter');
 
-$app->get('/{objectId}/annotation/{entityId}', 'IIIF.Annotation')
+$app->get('/iiif/{objectId}/annotation/{entityId}', 'IIIF.Annotation')
     ->setName('iiif.annotation')
     ->add('IIIF.Filter');
 
-$app->get('/{objectId}/sequence/{entityId}.json', 'IIIF.Sequence:asJSON')
+$app->get('/iiif/{objectId}/sequence/{entityId}.json', 'IIIF.Sequence:asJSON')
     ->setName('iiif.sequence.json')
     ->add('IIIF.Filter');
 
-$app->get('/{objectId}/sequence/{entityId}', 'IIIF.Sequence')
+$app->get('/iiif/{objectId}/sequence/{entityId}', 'IIIF.Sequence')
     ->setName('iiif.sequence')
     ->add('IIIF.Filter');
 
-$app->get('/{objectId}/image/{entityId}/info.json', 'IIIF.IIPImage:asJSON')
+$app->get('/iiif/{objectId}/image/{entityId}/info.json', 'IIIF.IIPImage:asJSON')
     ->setName('iiif.image.json')
     ->add('IIIF.Filter');
 
-$app->get('/{objectId}/image/{entityId}/{ops:.*}', 'IIIF.IIPImage:asJPEG')
+$app->get('/iiif/{objectId}/image/{entityId}/{ops:.*}', 'IIIF.IIPImage:asJPEG')
     ->setName('iiif.image.data')
     ->add('IIIF.Filter');
 
-$app->get('/{objectId}/image/{entityId}', 'IIIF.Image')
+$app->get('/iiif/{objectId}/image/{entityId}', 'IIIF.Image')
     ->setName('iiif.image')
     ->add('IIIF.Filter');
 
