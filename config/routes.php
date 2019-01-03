c88f6341cedf43e488496cf24be26962fd4bba67
David Maus <maus@hab.de>
2019-01-03 15:45:16 +0100

    Remove route names

--- config/routes.php
+++ config/routes.php
@@ -1,37 +1,29 @@
 <?php
 
-$app->get('/iiif/{objectId}/manifest.json', 'IIIF.Manifest:asJSON')
-    ->setName('iiif.manifest.json');
+$app->get('/iiif/{objectId}/manifest.json', 'IIIF.Manifest:asJSON');
 
-$app->get('/iiif/{objectId}/manifest', 'IIIF.NonInformationResource')
-    ->setName('iiif.manifest');
+$app->get('/iiif/{objectId}/manifest', 'IIIF.NonInformationResource');
 
-$app->get('/iiif/{objectId}/canvas/{entityId}.json', 'IIIF.Canvas:asJSON')
-    ->setName('iiif.canvas.json');
+$app->get('/iiif/{objectId}/canvas/{entityId}.json', 'IIIF.Canvas:asJSON');
 
-$app->get('/iiif/{objectId}/canvas/{entityId}', 'IIIF.NonInformationResource')
-    ->setName('iiif.canvas');
+$app->get('/iiif/{objectId}/canvas/{entityId}', 'IIIF.NonInformationResource');
 
-$app->get('/iiif/{objectId}/annotation/{entityId}.json', 'IIIF.Annotation:asJSON')
-    ->setName('iiif.annotation.json');
+$app->get('/iiif/{objectId}/annotation/{entityId}.json', 'IIIF.Annotation:asJSON');
 
-$app->get('/iiif/{objectId}/annotation/{entityId}', 'IIIF.NonInformationResource')
-    ->setName('iiif.annotation');
+$app->get('/iiif/{objectId}/annotation/{entityId}', 'IIIF.NonInformationResource');
 
-$app->get('/iiif/{objectId}/sequence/{entityId}.json', 'IIIF.Sequence:asJSON')
-    ->setName('iiif.sequence.json');
+$app->get('/iiif/{objectId}/sequence/{entityId}.json', 'IIIF.Sequence:asJSON');
 
-$app->get('/iiif/{objectId}/sequence/{entityId}', 'IIIF.NonInformationResource')
-    ->setName('iiif.sequence');
+$app->get('/iiif/{objectId}/sequence/{entityId}', 'IIIF.NonInformationResource');
 
-$app->get('/iiif/{objectId}/image/{entityId}/info.json', 'IIIF.Image:asJSON')
-    ->setName('iiif.image.json');
+$app->get('/iiif/{objectId}/image/{entityId}/info.json', 'IIIF.Image:asJSON');
 
-$app->get('/iiif/{objectId}/image/{entityId}/{ops:.*}', 'IIIF.Image:asJPEG')
-    ->setName('iiif.image.data');
+$app->get('/iiif/{objectId}/image/{entityId}.json', 'IIIF.Image:asJSON');
 
 $app->get('/iiif/{objectId}/image/{entityId}', 'IIIF.NonInformationResource')
     ->setName('iiif.image');
+
+$app->get('/iiif/{objectId}/image/{entityId}/{ops:.*}', 'IIIF.Image:asJPEG');
 
 $app->get('/iiif/', function ($req, $res, $args) {
     return $res->write('Nothing to see here...');
