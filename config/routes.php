5f3d30dc0b96fa9e37bcd44894bc9f9a8ffce069
David Maus <maus@hab.de>
2019-01-07 14:30:05 +0100

    Use dedicated route für objects

--- config/routes.php
+++ config/routes.php
@@ -1,27 +1,25 @@
 <?php
 
-$app->get('/iiif/{objectId}/image/{entityId}.json', 'IIIF.Image:getImageInfo')
+$app->get('/iiif/object/{objectId}/image/{entityId}.json', 'IIIF.Image:getImageInfo')
     ->add('IIIF.InformationResource.JSON');
-$app->get('/iiif/{objectId}/image/{entityId}/info.json', 'IIIF.Image:getImageInfo')
+$app->get('/iiif/object/{objectId}/image/{entityId}/info.json', 'IIIF.Image:getImageInfo')
     ->add('IIIF.InformationResource.JSON');
-$app->get('/iiif/{objectId}/image/{entityId}/{ops:.*}', 'IIIF.Image:getImageStream');
+$app->get('/iiif/object/{objectId}/image/{entityId}/{ops:.*}', 'IIIF.Image:getImageStream');
 
-$app->get('/iiif/{objectId}/image/{entityId}', 'IIIF.NonInformationResource')
+$app->get('/iiif/object/{objectId}/image/{entityId}', 'IIIF.NonInformationResource')
     ->setName('iiif.image');
 
-$app->get('/iiif/{objectId}/{entityType}.json', 'IIIF.Presentation')
+$app->get('/iiif/object/{objectId}/{entityType}.json', 'IIIF.Presentation')
     ->add('IIIF.InformationResource.JSON');
 
-$app->get('/iiif/{objectId}/{entityType}', 'IIIF.NonInformationResource');
+$app->get('/iiif/object/{objectId}/{entityType}', 'IIIF.NonInformationResource');
 
-$app->get('/iiif/{objectId}/{entityType}/{entityId}.json', 'IIIF.Presentation')
+$app->get('/iiif/object/{objectId}/{entityType}/{entityId}.json', 'IIIF.Presentation')
         ->add('IIIF.InformationResource.JSON');
 
-$app->get('/iiif/{objectId}/{entityType}/{entityId}', 'IIIF.NonInformationResource');
+$app->get('/iiif/object/{objectId}/{entityType}/{entityId}', 'IIIF.NonInformationResource');
 
-
-
-$app->get('/iiif/', function ($req, $res, $args) {
+$app->get('/iiif/object/', function ($req, $res, $args) {
     return $res->write('Nothing to see here...');
 })->setName('iiif');
 
