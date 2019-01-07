1d3547d8b5c682460cde8d60cfc3072e19025955
David Maus <maus@hab.de>
2019-01-07 14:21:42 +0100

    Rewrite Image API controller

--- src/IIIF/Image.php
+++ src/IIIF/Image.php
@@ -17,82 +17,42 @@
  * along with HAB Diglib IIIF.  If not, see <https://www.gnu.org/licenses/>.
  *
  * @author    David Maus <maus@hab.de>
- * @copyright (c) 2018 by Herzog August Bibliothek Wolfenbüttel
+ * @copyright (c) 2019 by Herzog August Bibliothek Wolfenbüttel
  * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public License v3 or higher
  */
 
 namespace HAB\Diglib\API\IIIF;
 
-use HAB\Diglib\API\Error;
-
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 use Slim\Interfaces\RouterInterface as Router;
-
 use Slim\Http\Stream;
 
+use HAB\Diglib\API\Error;
+
+use RuntimeException;
+
 /**
- * IIIF Image API implementation.
+ * Image API controller.
  *
  * @author    David Maus <maus@hab.de>
- * @copyright (c) 2018 by Herzog August Bibliothek Wolfenbüttel
+ * @copyright (c) 2019 by Herzog August Bibliothek Wolfenbüttel
  * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public License v3 or higher
  */
-class Image extends Controller
+class Image
 {
-    protected static $baseRoute = 'iiif.image';
-
     private $server;
+    private $mapper;
 
-    public function __construct (Router $router, MapperFactory $mapper, ImageServer $server)
+    public function __construct (ImageServer $server, MapperFactory $mapper, Router $router)
     {
-        parent::__construct($router, $mapper);
         $this->server = $server;
+        $this->mapper = $mapper;
+        $this->router = $router;
     }
 
-    public function asJSON (Request $request, Response $response, array $arguments)
-    {
-        $response = parent::asJSON($request, $response, $arguments);
-        if ($complianceLevelUri = $this->server->getComplianceLevel()) {
-            $response = $response->withHeader('Link', sprintf('<%s>; rel="profile"', $complianceLevelUri));
-        }
-        return $response;
-    }
-
-    public function asJPEG (Request $request, Response $response, array $arguments)
-    {
-        $accept = array('image/jpeg');
-        $ctype = $this->findRequestedEntityContentType($request, $accept);
-        if (!$ctype) {
-            throw new Error\Http(406, array('Accept' => $accept));
-        }
-
-        $imageUri = $this->resolveImageUri($arguments['objectId'], $arguments['entityId']);
-        if (!file_exists($imageUri) || !is_readable($imageUri)) {
-            throw new Error\Http(404);
-        }
-
-        try {
-            $image = $this->server->getImageStream($imageUri, $arguments['ops']);
-        } catch (ImageServer\UnsupportedFeature $e) {
-            throw new Error\Http(400, array(), $e);
-        }
-        if (!is_resource($image)) {
-            throw new RuntimeException();
-        }
-
-        if ($complianceLevelUri = $this->server->getComplianceLevel()) {
-            $response = $response->withHeader('Link', sprintf('<%s>; rel="profile"', $complianceLevelUri));
-        }
-
-        return $response
-            ->withStatus(200)
-            ->withHeader('Content-Type', 'image/jpeg')
-            ->withBody(new Stream($image));
-    }
-
-    protected function getJSON (array $arguments)
+    public function getImageInfo (Request $request, Response $response, array $arguments)
     {
         $imageUri = $this->resolveImageUri($arguments['objectId'], $arguments['entityId']);
         if (!file_exists($imageUri) || !is_readable($imageUri)) {
@@ -105,19 +65,50 @@ class Image extends Controller
         $profile['supports'] []= 'jsonldMediatype';
         $profile['supports'] []= 'profileLinkHeader';
 
-        $info = $this->server->getImageInfo($imageUri);
-        $info['@id'] = $this->router->pathFor(static::$baseRoute, $arguments);
+        try {
+            $info = $this->server->getImageInfo($imageUri);
+        } catch (RuntimeException $e) {
+            throw new Error\Http(404);
+        }
+
+        $info['@id'] = $this->router->pathFor('iiif.image', $arguments);
         $info['profile'] = array($this->server->getComplianceLevel(), $profile);
-        return $this->encodeJSON($info);
+        
+        $payload = json_encode($info, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new RuntimeException(json_last_error_msg());
+        }
+        return $response->write($payload);
     }
 
-    protected function resolveImageUri ($objectId, $entityId)
+    public function getImageStream (Request $request, Response $response, array $arguments)
     {
-        $mapper = $this->getMapper($objectId);
-        $imageUri = $mapper->getImageUri($entityId);
+        $imageUri = $this->resolveImageUri($arguments['objectId'], $arguments['entityId']);
+        if (!file_exists($imageUri) || !is_readable($imageUri)) {
+            throw new Error\Http(404);
+        }
 
-        $location = $this->mapper->getObjectLocation($objectId);
+        try {
+            $image = $this->server->getImageStream($imageUri, $arguments['ops']);
+        } catch (ImageServer\UnsupportedFeature $e) {
+            throw new Error\Http(400, array(), $e);
+        }
 
-        return rtrim($location, '/\\')  . DIRECTORY_SEPARATOR . $imageUri;
+        if ($complianceLevelUri = $this->server->getComplianceLevel()) {
+            $response = $response->withHeader('Link', sprintf('<%s>; rel="profile"', $complianceLevelUri));
+        }
+
+        $response = $response->withHeader('Content-Type', $image->getMediatype());
+        $response = $response->withBody(new Stream($image->getStream()));
+        return $response;
     }
+
+    private function resolveImageUri ($objectId, $entityId)
+    {
+        $location = $this->mapper->getObjectLocation($objectId);
+        $mapper = $this->mapper->create($objectId);
+        $image = $mapper->getImageUri($entityId);
+        return rtrim($location, '/\\') . DIRECTORY_SEPARATOR . $image;
+    }
+
 }
