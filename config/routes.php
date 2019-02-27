f5b42686f0427033013ff4830da65cac6b0d4ab6
David Maus <maus@hab.de>
2019-02-27 08:56:25 +0100

    Change routes

--- config/routes.php
+++ config/routes.php
@@ -1,30 +1,30 @@
 <?php
 
-$app->get('/iiif/object/{objectId}/image/{entityId}.json', 'IIIF.Image:getImageInfo')
+$app->get('/object/{objectId}/image/{entityId}.json', 'IIIF.Image:getImageInfo')
     ->add('IIIF.InformationResource.JSON');
-$app->get('/iiif/object/{objectId}/image/{entityId}/info.json', 'IIIF.Image:getImageInfo')
+$app->get('/object/{objectId}/image/{entityId}/info.json', 'IIIF.Image:getImageInfo')
     ->add('IIIF.InformationResource.JSON');
-$app->get('/iiif/object/{objectId}/image/{entityId}/{ops:.*}', 'IIIF.Image:getImageStream');
+$app->get('/object/{objectId}/image/{entityId}/{ops:.*}', 'IIIF.Image:getImageStream');
 
-$app->get('/iiif/object/{objectId}/image/{entityId}', 'IIIF.NonInformationResource')
+$app->get('/object/{objectId}/image/{entityId}', 'IIIF.NonInformationResource')
     ->setName('iiif.image');
 
-$app->get('/iiif/object/{objectId}/{entityType}.json', 'IIIF.Presentation')
+$app->get('/object/{objectId}/{entityType}.json', 'IIIF.Presentation')
     ->add('IIIF.InformationResource.JSON');
 
-$app->get('/iiif/object/{objectId}/{entityType}', 'IIIF.NonInformationResource');
+$app->get('/object/{objectId}/{entityType}', 'IIIF.NonInformationResource');
 
-$app->get('/iiif/object/{objectId}/{entityType}/{entityId}.json', 'IIIF.Presentation')
+$app->get('/object/{objectId}/{entityType}/{entityId}.json', 'IIIF.Presentation')
         ->add('IIIF.InformationResource.JSON');
 
-$app->get('/iiif/object/{objectId}/{entityType}/{entityId}', 'IIIF.NonInformationResource');
+$app->get('/object/{objectId}/{entityType}/{entityId}', 'IIIF.NonInformationResource');
 
-$app->get('/iiif/object/', function ($req, $res, $args) {
+$app->get('/object/', function ($req, $res, $args) {
     return $res->write('Nothing to see here...');
 })->setName('iiif');
 
-$app->get('/iiif/collection/project/mssox', 'IIIF.NonInformationResource');
-$app->get('/iiif/collection/project/mssox.json', 'IIIF.StaticCollection')
+$app->get('/collection/project/mssox', 'IIIF.NonInformationResource');
+$app->get('/collection/project/mssox.json', 'IIIF.StaticCollection')
     ->add('IIIF.InformationResource.JSON');
 
 $app->add('Slim.RouterBasePath');
